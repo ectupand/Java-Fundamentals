8596f8c282fcef9004f92daa98c4a8123c338d12
ectupand <ruhnoita892@gmail.com>
2023-12-17 01:15:00 +0500

    fix array

--- src/main/java/interfaces/App.java
+++ src/main/java/interfaces/App.java
@@ -4,19 +4,14 @@ import interfaces.readables.Book;
 import interfaces.readables.Magazine;
 import interfaces.readables.Printable;
 
-import static interfaces.readables.Book.printBooks;
-
 public class App {
     public static void main(String[] args) {
-        Printable[] readables = new Printable[4];
-        Book book1 = new Book("Bookie");
-        Magazine magazine1 = new Magazine("Journalius");
-        Magazine magazine2 = new Magazine("Jurnal");
-        Book book2 = new Book("Boochok");
-        readables[0] = book1;
-        readables[1] = magazine1;
-        readables[2] = magazine2;
-        readables[3] = book2;
+        Printable[] readables = new Printable[] {
+                new Book("Bookie"),
+                new Magazine("Journalius"),
+                new Magazine("Jurnal"),
+                new Book("Boochok")
+        };
         for (Printable readable : readables){
             readable.print();
         }
